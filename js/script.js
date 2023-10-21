1baf07e36eedcb1284710fe15d3752bbf679497a
Abdul Samad M J <samadmj4@gmail.com>
2023-10-21 13:24:50 +0400

    side toast added

--- js/script.js
+++ js/script.js
@@ -25,3 +25,21 @@ const newsLetter = () => {
         });
     }
 }
+
+function closeToast() {
+    let toast = document.getElementById("toast");
+    toast.style.transform = "translateX(-35rem)";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let x;
+    let toast = document.getElementById("toast");
+    function showToast() {
+        // clearTimeout(x);
+        toast.style.transform = "translateX(0)";
+        // x = setTimeout(() => {
+        //     toast.style.transform = "translateX(400px)"
+        // }, 4000);
+    }
+    showToast();
+});

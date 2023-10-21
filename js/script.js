d26e17597cf42d1c35d4df2e591086ce6fe31c7a
Abdul Samad M J <samadmj4@gmail.com>
2023-10-21 11:38:26 +0400

    footer newsletter form set

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 var submitted = false;
-
+var subscribed = false;
 const contactUs = () => {
     if (submitted) {
         const ok = Swal.fire({
             title: 'Email Sent Successfully',
             text: 'We will get back to you through email and mobile soon, Thank You.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        }).then(() => {
+            window.location = '/';
+        });
+    }
+}
+
+const newsLetter = () => {
+    if (subscribed) {
+        const ok = Swal.fire({
+            title: 'Subscribed Successfully',
+            text: 'Thank you for subscribing to our newsletter.',
             icon: 'success',
             confirmButtonText: 'OK'
         }).then(() => {
